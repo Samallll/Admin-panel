1567e830128065a9617447ef7d962dbbe1e6fd16
Samallll <samalchokli@gmail.com>
2023-08-18 20:25:29 +0530

    Login page design completed

---
+++ src/main/java/com/example/as/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.example.as.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+	@GetMapping("/")
+	public String userHome() {
+		return "userHome.html";
+	}
+}
