32e6363bc815484faa15a694173e778e91b546b7
Samallll <samalchokli@gmail.com>
2023-08-20 19:23:09 +0530

    USER role is not adding in the database due to the inavailability of data in Role relation

--- src/main/java/com/example/as/service/UserService.java
+++ src/main/java/com/example/as/service/UserService.java
@@ -39,7 +39,16 @@ public class UserService implements UserDetailsService{
 	
 	public ApplicationUser registerUser(String userName,String emailId, String password) {
 		
-		Role userRole = roleRepository.findByAuthority("USER").get();
+//		Made as comments to check the working of registerUser
+//		Role userRole = roleRepository.findByAuthority("USER").get();
+//		
+//		Set<Role> roles = new HashSet<Role>();
+//		roles.add(userRole);
+		System.out.println("Registering");
+		
+		Role userRole = new Role("USER");
+		
+		System.out.println(roleRepository.count());
 		
 		Set<Role> roles = new HashSet<Role>();
 		roles.add(userRole);
