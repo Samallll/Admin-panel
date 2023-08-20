ce0d61799520bb8ceb772f1ca610ef3abcf7019d
Samallll <samalchokli@gmail.com>
2023-08-20 14:14:28 +0530

    Created basic role and user repository interfaces

---
+++ src/main/java/com/example/as/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.as.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.as.model.ApplicationUser;
+
+public interface UserRepository extends JpaRepository<ApplicationUser,Integer> {
+
+}
